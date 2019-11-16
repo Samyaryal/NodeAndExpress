12f5835fde8cdc684eb6406aa830342fd14d5ed6
Samikshya <samikshyaaryal5@gmail.com>
2019-11-16 19:00:09 +0200

    adding css to the page and the component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="from-wrapper">
-        <h1>Create Account</h1>
+class App extends Component {
+  render() {
+
+    return (
+      <div className="wrapper">
+        <div className="from-wrapper">
+          <h1>Create Account</h1>
           <form onSubmit={this.handleSubmit} noValidate>
-            <div className= "firstName">
+            <div className="firstName">
               <label htmlFor="firstName">First Name</label>
-              <input type= "text" className="" placeholder="First Name" type="text" name ="firstName" noValidate
-              onChange = {this.handleChange}/>
+              <input type="text" className="" placeholder="First Name" type="text" name="firstName" noValidate
+                onChange={this.handleChange} />
+            </div>
+            <div className="lastName">
+              <label htmlFor="lastName">First Name</label>
+              <input type="text" className="" placeholder="Last Name" type="text" name="lastName" noValidate
+                onChange={this.handleChange} />
+            </div>
+            <div className="email">
+              <label htmlFor="email">Email</label>
+              <input type="text" className="" placeholder="Email" type="text" name="email" noValidate
+                onChange={this.handleChange} /> 
+            </div>
+            <div className="password">
+              <label htmlFor="password">Password</label>
+              <input type="text" className="" placeholder="Password" type="text" name="password" noValidate
+                onChange={this.handleChange} />
             </div>
           </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
