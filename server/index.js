af0f62ab9a08b0211bd0f7d941fdbf9066b84340
Samikshya <samikshyaaryal5@gmail.com>
2019-12-02 19:45:06 +0200

    adding googleStrategy

--- server/index.js
+++ server/index.js
@@ -1,11 +1,33 @@
 const express = require('express');
-const passport = reequire('passport');
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy()); // create new instances of the google passport strategy
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        }, 
+        (accessToken, refreshToken, profile, done) => {
+            console.log('access token', accessToken);
+            console.log('refresh token', refreshToken);
+            console.log('profile', profile);
+        }
+    )
+); // create new instances of the google passport strategy
+
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT);
