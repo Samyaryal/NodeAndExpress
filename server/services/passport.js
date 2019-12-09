55048854e0a15538f2366f774ad83e0c1eecf71b
Samikshya <samikshyaaryal5@gmail.com>
2019-12-09 21:54:53 +0200

    encoding users and adding deserialize users

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,18 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+       .then(user => {
+           done(null, user);
+       });
+});
+
+
 passport.use(
     new GoogleStrategy(
         {
@@ -13,7 +25,18 @@ passport.use(
             callbackURL: '/auth/google/callback'
         }, 
         (accessToken, refreshToken, profile, done) => {
-            new User ({ googleId: profile.id}).save(); // id from users google profile // .save will save the instances to the db
+            User.findOne({ googleId: profile.id})
+                .then((existingUser) => {
+                    if (existingUser){
+                        done(null, existingUser);
+                        // already hav ethe record of the given profile Id 
+                    }else {
+                        new User ({ googleId: profile.id}).save()
+                            .then(user => done(null, user)); // id from users google profile // .save will save the instances to the db
+                        // don't have a user record  
+                    }
+                }) 
+        
         }
     )
 ); // create new instances of the google passport strategy
