4291b32cc0c99bc38f4e739a1e73fcd2734e6ae9
Samikshya <samikshyaaryal5@gmail.com>
2019-12-09 10:27:09 +0200

    Adding a userSchema and creating a new class.

--- server/models/User.js
+++ server/models/User.js
@@ -1,2 +1,9 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema ({
+    googleId: String
+
+});
+mongoose.model('users', userSchema) // telling mongoose that we want to create a new collection called users.
