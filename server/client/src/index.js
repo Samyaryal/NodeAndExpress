ee9bfad5d7f3ad0f3bdaab6731c3141ee3e9f15b
Samikshya <samikshyaaryal5@gmail.com>
2019-12-11 21:34:23 +0200

    working on fixing errors about react hook

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-
 import App from './components/App';
 
 const store = createStore(() => [] , {}, applyMiddleware());
 
 ReactDOM.render(
-    <Provider store = {store}> <App /></Provider>,
-    document.querySelector('#root'));
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.querySelector("#root")
+);
