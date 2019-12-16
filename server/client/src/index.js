1b3006bf1a73224749fbd3f69757df02edff8731
Samikshya <samikshyaaryal5@gmail.com>
2019-12-16 23:49:07 +0200

    cannot find the errors about the wrong hook

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-
 import App from './components/App';
 
 const store = createStore(() => [] , {}, applyMiddleware());
