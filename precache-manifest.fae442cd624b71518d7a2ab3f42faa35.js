597370c07ea9360de7f739cf4142c0f28a8b2e6b
Samikshya <samikshyaaryal5@gmail.com>
2020-12-17 15:43:57 +0200

    Updates

---
+++ precache-manifest.fae442cd624b71518d7a2ab3f42faa35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4dd9507e6a7d9738f1c7aad63a1906f",
+    "url": "/React_form_validation/index.html"
+  },
+  {
+    "revision": "a269201e544cbadbadd9",
+    "url": "/React_form_validation/static/css/main.d676d32f.chunk.css"
+  },
+  {
+    "revision": "80b965bdd761de465d59",
+    "url": "/React_form_validation/static/js/2.70c20db4.chunk.js"
+  },
+  {
+    "revision": "a269201e544cbadbadd9",
+    "url": "/React_form_validation/static/js/main.32e12b80.chunk.js"
+  },
+  {
+    "revision": "588ab006db759b833de7",
+    "url": "/React_form_validation/static/js/runtime-main.6205b6d5.js"
+  }
+]);
